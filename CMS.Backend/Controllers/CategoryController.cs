d1f1783ed9deb845fa6d118d8d0145a903df7048
tthao151196 <tthao151196@gmail.com>
2026-05-15 15:18:07 +0700

    Hoàn thành bước 5-buổi 1

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities; // Kết nối tới lớp dữ liệu bạn vừa tạo
+
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        // Tạo danh sách dữ liệu mẫu trực tiếp trong code
+        var list = new List<Category> {
+            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
+            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
+        };
+        return View(list); // Gửi danh sách này sang giao diện
+    }
+}
