5cca7f05466d7ffbd65fdf3c5e3dc1eadbd598a8
tthao151196 <tthao151196@gmail.com>
2026-05-17 11:26:10 +0700

    Buoi 02 - Bai tap ren luyen

---
+++ CMS.Backend/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using CMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CMS.Backend.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Orders.ToList();
+            return View(data);
+        }
+    }
+}
